3f25836a3ad7d4b8334a9b11ebc6a0a7b3e6cdd3
Edgar Green <zfz2020@foxmail.com>
2019-03-20 13:41:14 +0800

    feat(util): add sql date converter for local date and local date time

---
+++ src/main/java/cn/varfunc/restaurant/util/LocalDateSqlDateConverter.java
@@ -0,0 +1,19 @@
+package cn.varfunc.restaurant.util;
+
+import javax.persistence.AttributeConverter;
+import javax.persistence.Converter;
+import java.sql.Date;
+import java.time.LocalDate;
+
+@Converter(autoApply = true)
+public class LocalDateSqlDateConverter implements AttributeConverter<LocalDate, Date> {
+    @Override
+    public Date convertToDatabaseColumn(LocalDate attribute) {
+        return Date.valueOf(attribute);
+    }
+
+    @Override
+    public LocalDate convertToEntityAttribute(Date dbData) {
+        return dbData.toLocalDate();
+    }
+}
---
+++ src/main/java/cn/varfunc/restaurant/util/LocalDateTimeSqlTimestampConverter.java
@@ -0,0 +1,19 @@
+package cn.varfunc.restaurant.util;
+
+import javax.persistence.AttributeConverter;
+import javax.persistence.Converter;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+
+@Converter(autoApply = true)
+public class LocalDateTimeSqlTimestampConverter implements AttributeConverter<LocalDateTime, Timestamp> {
+    @Override
+    public Timestamp convertToDatabaseColumn(LocalDateTime attribute) {
+        return Timestamp.valueOf(attribute);
+    }
+
+    @Override
+    public LocalDateTime convertToEntityAttribute(Timestamp dbData) {
+        return dbData.toLocalDateTime();
+    }
+}
