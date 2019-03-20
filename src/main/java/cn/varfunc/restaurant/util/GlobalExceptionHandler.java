f0c74857738c7b5c4fc4e86389ca0b5cf64a8dcd
Edgar Green <zfz2020@foxmail.com>
2019-03-20 17:01:06 +0800

    refactor(globalExceptionHandler): improve exception handler

--- src/main/java/cn/varfunc/restaurant/util/GlobalExceptionHandler.java
+++ src/main/java/cn/varfunc/restaurant/util/GlobalExceptionHandler.java
@@ -1,10 +1,14 @@
 package cn.varfunc.restaurant.util;
 
+import cn.varfunc.restaurant.domain.response.ApiResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
+import java.util.Arrays;
 import java.util.NoSuchElementException;
 
 @Slf4j
@@ -12,12 +16,11 @@ import java.util.NoSuchElementException;
 public class GlobalExceptionHandler {
     @ExceptionHandler(NoSuchElementException.class)
     @ResponseBody
-    public NoSuchElementException handleException(NoSuchElementException e) {
-        log.info("{}", e);
-        return e;
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ApiResponse handleNoSuchElementExceptionException(NoSuchElementException e) {
+        log.info("{}", Arrays.toString(e.getStackTrace()));
+        return ApiResponse.builder()
+                .message(e.getMessage())
+                .build();
     }
-
-    // TODO: 2019/3/5 Improve Exception Handler
-
-    // TODO: 2019/3/5 Standardize all of the response message format
 }
