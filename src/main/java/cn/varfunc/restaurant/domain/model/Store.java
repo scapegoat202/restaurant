825245f95dbe2beabc1134bd1184fa921035d2b0
Edgar Green <zfz2020@foxmail.com>
2019-03-20 13:44:53 +0800

    chore(model): optimize imports

--- src/main/java/cn/varfunc/restaurant/domain/model/Store.java
+++ src/main/java/cn/varfunc/restaurant/domain/model/Store.java
@@ -1,9 +1,6 @@
 package cn.varfunc.restaurant.domain.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 import lombok.experimental.Accessors;
 
