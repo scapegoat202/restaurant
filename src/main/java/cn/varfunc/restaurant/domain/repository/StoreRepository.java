2312b0bb48ba40019db30dd589e50495c5aea312
Edgar Green <zfz2020@foxmail.com>
2019-03-21 14:02:43 +0800

    feat(domain/repository): add new instance method for login validation

--- src/main/java/cn/varfunc/restaurant/domain/repository/StoreRepository.java
+++ src/main/java/cn/varfunc/restaurant/domain/repository/StoreRepository.java
@@ -3,8 +3,11 @@ package cn.varfunc.restaurant.domain.repository;
 import cn.varfunc.restaurant.domain.model.Store;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 /**
  * Store Repository
  */
 public interface StoreRepository extends JpaRepository<Store, Long> {
+    Optional<Store> findByUsername(String username);
 }
