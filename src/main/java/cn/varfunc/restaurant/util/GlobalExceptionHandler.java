ce03c10ffb0edd52182c0e0f4f3aa113f578e740
Edgar Green <zfz2020@foxmail.com>
2019-03-21 13:26:36 +0800

    refactor(util): code reformat

--- src/main/java/cn/varfunc/restaurant/util/GlobalExceptionHandler.java
+++ src/main/java/cn/varfunc/restaurant/util/GlobalExceptionHandler.java
@@ -1,24 +1,20 @@
 package cn.varfunc.restaurant.util;
 
 import cn.varfunc.restaurant.domain.response.ApiResponse;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-import java.util.Arrays;
 import java.util.NoSuchElementException;
 
-@Slf4j
 @ControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(NoSuchElementException.class)
     @ResponseBody
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public ApiResponse handleNoSuchElementExceptionException(NoSuchElementException e) {
-        log.info("{}", Arrays.toString(e.getStackTrace()));
         return ApiResponse.builder()
                 .message(e.getMessage())
                 .build();
