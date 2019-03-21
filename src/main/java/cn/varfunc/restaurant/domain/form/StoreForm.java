06b75f259d9521b905bfded244f6344af9532bba
Edgar Green <zfz2020@foxmail.com>
2019-03-21 14:33:52 +0800

    refactor(domain/form): remove unused annotation

--- src/main/java/cn/varfunc/restaurant/domain/form/StoreForm.java
+++ src/main/java/cn/varfunc/restaurant/domain/form/StoreForm.java
@@ -3,11 +3,8 @@ package cn.varfunc.restaurant.domain.form;
 import cn.varfunc.restaurant.domain.model.Address;
 import lombok.Data;
 
-import javax.validation.constraints.NotBlank;
-
 @Data
 public class StoreForm {
-    @NotBlank
     private String name;
     private String phoneNumber;
     private String announcement;
