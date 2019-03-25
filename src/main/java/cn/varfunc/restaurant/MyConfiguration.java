485fe7489a63aaaf4adfcf9588791e15e7500772
Edgar Green <zfz2020@foxmail.com>
2019-03-25 09:55:13 +0800

    fix(config): add cors support for patch

--- src/main/java/cn/varfunc/restaurant/MyConfiguration.java
+++ src/main/java/cn/varfunc/restaurant/MyConfiguration.java
@@ -14,7 +14,7 @@ public class MyConfiguration {
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
                         .allowedOrigins("*")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE");
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH");
             }
 
         };
