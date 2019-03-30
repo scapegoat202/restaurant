bc2e4bca2427f9751256282131197231041400f2
Edgar Green <zfz2020@foxmail.com>
2019-03-30 19:42:23 +0800

    feat: add get file url method

--- src/main/java/cn/varfunc/restaurant/service/FileService.java
+++ src/main/java/cn/varfunc/restaurant/service/FileService.java
@@ -47,4 +47,34 @@ public class FileService {
             e.printStackTrace();
         }
     }
+
+    public String getFileURL(String bucketName, UUID uuid) {
+        String url = null;
+        //noinspection TryWithIdenticalCatches
+        try {
+            url = this.client.presignedGetObject(bucketName, uuid.toString());
+        } catch (InvalidBucketNameException e) {
+            e.printStackTrace();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (InsufficientDataException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InvalidKeyException e) {
+            e.printStackTrace();
+        } catch (NoResponseException e) {
+            e.printStackTrace();
+        } catch (XmlPullParserException e) {
+            e.printStackTrace();
+        } catch (ErrorResponseException e) {
+            e.printStackTrace();
+        } catch (InternalException e) {
+            e.printStackTrace();
+        } catch (InvalidExpiresRangeException e) {
+            e.printStackTrace();
+        }
+        return url;
+    }
+
 }
