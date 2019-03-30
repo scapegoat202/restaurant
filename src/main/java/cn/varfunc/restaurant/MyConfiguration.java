ce7b839ad4c0e8e783c1d59845ec869ba63ad473
Edgar Green <zfz2020@foxmail.com>
2019-03-30 19:19:42 +0800

    refactor: move configuration to properties file

--- src/main/java/cn/varfunc/restaurant/MyConfiguration.java
+++ src/main/java/cn/varfunc/restaurant/MyConfiguration.java
@@ -3,6 +3,7 @@ package cn.varfunc.restaurant;
 import io.minio.MinioClient;
 import io.minio.errors.InvalidEndpointException;
 import io.minio.errors.InvalidPortException;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -26,15 +27,15 @@ public class MyConfiguration {
     }
 
     @Bean
-    public MinioClient minioClient() {
+    public MinioClient minioClient(@Value("${cn.varfunc.restaurant.minio.endpoint}") String endpoint,
+                                   @Value("${cn.varfunc.restaurant.minio.access-key}") String accessKey,
+                                   @Value("${cn.varfunc.restaurant.minio.secret-key}") String secretKey) {
         if (minioClient != null) {
             return minioClient;
         }
 
         try {
-            minioClient = new MinioClient("https://play.minio.io:9000",
-                    "Q3AM3UQ867SPQQA43P2F",
-                    "zuf+tfteSlswRu7BJ86wekitnifILbZam1KYY3TG");
+            minioClient = new MinioClient(endpoint, accessKey, secretKey);
         } catch (InvalidEndpointException | InvalidPortException e) {
             e.printStackTrace();
         }
